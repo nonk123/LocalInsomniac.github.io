bf6b6bc76093ce4adba2daeba3640a38877b7fdc
Can't Sleep <localamnesiac@gmail.com>
2026-03-09 19:01:06 +0200

    Update gallery layout

--- www/gallery/render.lua
+++ www/gallery/render.lua
@@ -1,59 +1,53 @@
 render("gallery/_gallery.html", "gallery/index.html", {
-    years = {{
+    images = {{
+        id = "terraria_portraits",
+        name = "Terraria Portraits",
         year = "2026",
-
-        images = {{
-            id = "terraria_portraits",
-            name = "Terraria Portraits",
-            description = "Terraria NPC portraits made for <a href=\"https://steamcommunity.com/sharedfiles/filedetails/?id=3660032479\">Pointy Portraits</a><img src=\"/assets/icons/external.png\" />."
-        }}
+        description = "Terraria NPC portraits made for <a href=\"https://steamcommunity.com/sharedfiles/filedetails/?id=3660032479\">Pointy Portraits</a><img src=\"/assets/icons/external.png\" />."
     }, {
+        id = "marina_64",
+        name = "Marina 64",
         year = "2025",
-
-        images = {{
-            id = "marina_64",
-            name = "Marina 64",
-            description = "3D model of Marina made for a scrapped sm64coopdx character mod. Mario for comparison."
-        }, {
-            id = "witching_stone_girl",
-            name = "Witching Stone Girl",
-            description = "A character from Alexander Taylor's <a href=\"https://store.steampowered.com/app/2693530/Witching_Stone\">Witching Stone</a><img src=\"/assets/icons/external.png\" />, designed by <a href=\"https://www.artstation.com/artwork/49mlGq\">Sam Miller</a><img src=\"/assets/icons/external.png\" />."
-        }}
+        description = "3D model of Marina made for a scrapped sm64coopdx character mod. Mario for comparison."
     }, {
+        id = "witching_stone_girl",
+        name = "Witching Stone Girl",
+        year = "2025",
+        description = "A character from Alexander Taylor's <a href=\"https://store.steampowered.com/app/2693530/Witching_Stone\">Witching Stone</a><img src=\"/assets/icons/external.png\" />, designed by <a href=\"https://www.artstation.com/artwork/49mlGq\">Sam Miller</a><img src=\"/assets/icons/external.png\" />."
+    }, {
+        id = "mecha_marina",
+        name = "Mecha Marina",
         year = "2024",
-
-        images = {{
-            id = "mecha_marina",
-            name = "Mecha Marina",
-            description = "Custom design for Marina Liteyears, including her human form."
-        }, {
-            id = "jazzpunk",
-            name = "jazzpunk",
-            description = "Thumbnail for <a href=\"https://www.youtube.com/watch?v=nXaYyk4gP34\">jazzpunk</a><img src=\"/assets/icons/external.png\" />, a tool-assisted Duck Game montage. Background from Rav's BENEATH THE TOXIC JUNGLE.<br />Commissioned by <a href=\"https://www.youtube.com/@swagdaddyfresh\">Soh</a><img src=\"/assets/icons/external.png\" />."
-        }, {
-            id = "jackinthebox",
-            name = "jackinthebox",
-            description = "Thumbnail for <a href=\"https://www.youtube.com/watch?v=9Ouh1kBUxjE\">jackinthebox</a><img src=\"/assets/icons/external.png\" />, a Duck Game montage. Background from Majora's Mask 3D (Kill Bill: The Rapper and Airospace's MAJORA).<br />Commissioned by <a href=\"https://www.youtube.com/@swagdaddyfresh\">Soh</a><img src=\"/assets/icons/external.png\" />."
-        }, {
-            id = "jalter",
-            name = "jalter",
-            description = "Thumbnail for <a href=\"https://www.youtube.com/watch?v=ypM5BzYVsgc\">jalter</a><img src=\"/assets/icons/external.png\" />, a Duck Game montage. Background from no1mportant's There's No I.<br />Commissioned by <a href=\"https://www.youtube.com/@swagdaddyfresh\">Soh</a><img src=\"/assets/icons/external.png\" />."
-        }, {
-            id = "johnnygrey",
-            name = "johnnygrey",
-            description = "Thumbnail for <a href=\"https://www.youtube.com/watch?v=ZQvBmvhpHwI\">johnnygrey</a><img src=\"/assets/icons/external.png\" />, a Duck Game montage. Background from Scuare's Everything Is Meaningless If There's No Reference Point for Anything.<br />Commissioned by <a href=\"https://www.youtube.com/@swagdaddyfresh\">Soh</a><img src=\"/assets/icons/external.png\" />."
-        }}
+        description = "Custom design for Marina Liteyears, including her human form."
     }, {
+        id = "jazzpunk",
+        name = "jazzpunk",
+        year = "2024",
+        description = "Thumbnail for <a href=\"https://www.youtube.com/watch?v=nXaYyk4gP34\">jazzpunk</a><img src=\"/assets/icons/external.png\" />, a tool-assisted Duck Game montage. Background from Rav's BENEATH THE TOXIC JUNGLE.<br />Commissioned by <a href=\"https://www.youtube.com/@swagdaddyfresh\">Soh</a><img src=\"/assets/icons/external.png\" />."
+    }, {
+        id = "jackinthebox",
+        name = "jackinthebox",
+        year = "2024",
+        description = "Thumbnail for <a href=\"https://www.youtube.com/watch?v=9Ouh1kBUxjE\">jackinthebox</a><img src=\"/assets/icons/external.png\" />, a Duck Game montage. Background from Majora's Mask 3D (Kill Bill: The Rapper and Airospace's MAJORA).<br />Commissioned by <a href=\"https://www.youtube.com/@swagdaddyfresh\">Soh</a><img src=\"/assets/icons/external.png\" />."
+    }, {
+        id = "jalter",
+        name = "jalter",
+        year = "2024",
+        description = "Thumbnail for <a href=\"https://www.youtube.com/watch?v=ypM5BzYVsgc\">jalter</a><img src=\"/assets/icons/external.png\" />, a Duck Game montage. Background from no1mportant's There's No I.<br />Commissioned by <a href=\"https://www.youtube.com/@swagdaddyfresh\">Soh</a><img src=\"/assets/icons/external.png\" />."
+    }, {
+        id = "johnnygrey",
+        name = "johnnygrey",
+        year = "2024",
+        description = "Thumbnail for <a href=\"https://www.youtube.com/watch?v=ZQvBmvhpHwI\">johnnygrey</a><img src=\"/assets/icons/external.png\" />, a Duck Game montage. Background from Scuare's Everything Is Meaningless If There's No Reference Point for Anything.<br />Commissioned by <a href=\"https://www.youtube.com/@swagdaddyfresh\">Soh</a><img src=\"/assets/icons/external.png\" />."
+    }, {
+        id = "sjac",
+        name = "[s.j.a.c.]",
         year = "2022",
-
-        images = {{
-            id = "sjac",
-            name = "[s.j.a.c.]",
-            description = "Thumbnail for <a href=\"https://www.youtube.com/watch?v=YNX9-rPSXPs\">[s.j.a.c.]</a><img src=\"/assets/icons/external.png\" />, a Duck Game montage. Based on Gorillaz' Demon Days.<br />Commissioned by <a href=\"https://www.youtube.com/@swagdaddyfresh\">Soh</a><img src=\"/assets/icons/external.png\" />."
-        }, {
-            id = "mario_and_luigi_rpg",
-            name = "Mario & Luigi RPG",
-            description = "Based on <a href=\"https://isa_happyfield.artstation.com/projects/0XkVQG\">Isa Happyfield's concept art for Mario & Luigi: Superstar Saga</a><img src=\"/assets/icons/external.png\" />."
-        }}
+        description = "Thumbnail for <a href=\"https://www.youtube.com/watch?v=YNX9-rPSXPs\">[s.j.a.c.]</a><img src=\"/assets/icons/external.png\" />, a Duck Game montage. Based on Gorillaz' Demon Days.<br />Commissioned by <a href=\"https://www.youtube.com/@swagdaddyfresh\">Soh</a><img src=\"/assets/icons/external.png\" />."
+    }, {
+        id = "mario_and_luigi_rpg",
+        name = "Mario & Luigi RPG",
+        year = "2022",
+        description = "Based on <a href=\"https://isa_happyfield.artstation.com/projects/0XkVQG\">Isa Happyfield's concept art for Mario & Luigi: Superstar Saga</a><img src=\"/assets/icons/external.png\" />."
     }}
 });
